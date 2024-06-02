307651dd0468274cc125153c3e95f3716d4f7064
Ulker Huseynova <ulkerhc@gmail.com>
2024-06-02 02:48:11 +0200

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        //version1
+        System.out.println("hello GIT");
+        System.out.println("git add. staging area ya alir" );
+
+    }
+}
